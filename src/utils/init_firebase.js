a0ed8d1de6a417c1daeab180e1280b50c80e6394
amenolabode <olabodeamen4@gmail.com>
2023-08-15 07:47:15 +0000

    firebase backend bug

--- src/utils/init_firebase.js
+++ src/utils/init_firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  authDomain: "sparkdeals-a9107.firebaseapp.com",
+  projectId: "sparkdeals-a9107",
+  storageBucket: "sparkdeals-a9107.appspot.com",
+  messagingSenderId: "697702771938",
+  appId: "1:697702771938:web:054736840b4fcb19af42bb",
+  measurementId: "G-RJ866G4KDV",
 };
 
 // Initialize Firebase
