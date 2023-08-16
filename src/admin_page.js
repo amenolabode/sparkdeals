d8dab685a7afff44abbdbbec9522ff2a5f7b96fd
amenolabode <olabodeamen4@gmail.com>
2023-08-16 14:34:51 +0000

    admin page deals update

--- src/admin_page.js
+++ src/admin_page.js
@@ -7,7 +7,7 @@ import { db, storage } from "./utils/init_firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { v4 } from "uuid";
 import { Modal, Drawer, DatePicker } from "antd";
-import { FaEllipsisV, FaExclamationCircle } from "react-icons/fa";
+import { FaEllipsisV, FaExclamationCircle, FaPlus } from "react-icons/fa";
 
 const AdminPage = () => {
   const [productName, setProductName] = useState("");
@@ -22,7 +22,8 @@ const AdminPage = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [useDrawer, setUseDrawer] = useState(false);
   const [activePage, setActivePage] = useState("deals");
-  const [expiryDate, setExpiryDate] = useState("deals");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [startDate, setStartDate] = useState("");
   const [menuVisible, setMenuVisible] = useState(false);
   const [menuToggle, setMenuToggle] = useState("");
   const [modalVisible, setModalVisible] = useState("");
@@ -41,7 +42,11 @@ const AdminPage = () => {
     setModalData(data);
   };
 
-  const handleDateChange = (dateString) => {
+  const handleStartDateChange = (dateString) => {
+    setStartDate(dateString);
+
+  };
+  const handleEndDateChange = (dateString) => {
     setExpiryDate(dateString);
   };
 
@@ -116,6 +121,8 @@ const AdminPage = () => {
     });
   };
 
+  const discount = Math.floor(100 - (newPrice / oldPrice) * 100);
+
   const handleSubmit = () => {
     handleFile()
       .then((imageURL) => {
@@ -125,9 +132,11 @@ const AdminPage = () => {
           productName,
           oldPrice,
           expiryDate,
+          startDate,
           currentPrice: newPrice,
           availableQTY: quantity,
           measurement: unit,
+          discount,
         }).then(() => {
           setProductName("");
           setOldPrice(0);
@@ -161,6 +170,15 @@ const AdminPage = () => {
     };
   }, [screenWidth]);
 
+  const formatDate = (inputDate) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const formattedDate = new Date(inputDate).toLocaleDateString(
+      undefined,
+      options
+    );
+    return formattedDate;
+  };
+
   const isAdditionValid =
     productName !== "" &&
     oldPrice !== 0 &&
@@ -168,106 +186,125 @@ const AdminPage = () => {
     quantity !== 0 &&
     unit !== "" &&
     image !== null &&
-    expiryDate !== "";
+    expiryDate !== "" &&
+    startDate !== "";
 
   const AddDealForm = () => {
-    return (<div className="mx-auto">
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500">Product Image</label>
-      </div>
-      <Input
-        type="file"
-        name="image"
-        onChange={(e) => {
-          setImage(e.target.files[0]);
-        }}
-      ></Input>
-    </div>
+    return (
+      <div className="mx-auto">
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500">Product Image</label>
+          </div>
+          <Input
+            type="file"
+            name="image"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+          ></Input>
+        </div>
 
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500"> Product Name</label>
-      </div>
-      <Input
-        className="w-full h-[48px] hover:border-green-500 active:border-green-600"
-        placeholder="Please enter Product Name"
-        value={productName}
-        required={true}
-        onChange={(e) => setProductName(e.target.value)}
-      />
-    </div>
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500"> Old (Market) Price </label>
-      </div>
-      <Input
-        className="w-full h-[48px] hover:border-green-500 active:border-green-600"
-        placeholder="Old Price"
-        value={oldPrice}
-        required={true}
-        onChange={(e) => setOldPrice(e.target.value)}
-      />
-    </div>
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500"> New (Spark) Price </label>
-      </div>
-      <Input
-        className="w-full h-[48px] hover:border-green-500 active:border-green-600"
-        placeholder="New Price"
-        value={newPrice}
-        required={true}
-        onChange={(e) => setNewPrice(e.target.value)}
-      />
-    </div>
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500"> Available Quantity </label>
-      </div>
-      <Input
-        className="w-full h-[48px] hover:border-green-500 active:border-green-600"
-        placeholder="Available Quantity"
-        value={quantity}
-        required={true}
-        onChange={(e) => setQuantity(e.target.value)}
-      />
-    </div>
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500"> Unit (Kg, Pieces, Packs) </label>
-      </div>
-      <Input
-        className="w-full h-[48px] hover:border-green-500 active:border-green-600"
-        placeholder="Kg, Pieces, Packs"
-        value={unit}
-        required={true}
-        onChange={(e) => setUnit(e.target.value)}
-      />
-    </div>
-    <div className="mt-4">
-      <div className="mb-1">
-        <label className="text-gray-500"> Deal Expiry Date</label>
-      </div>
-      <DatePicker
-        className="w-full"
-        onChange={(date, dateString) => handleDateChange(dateString)}
-      />
-    </div>
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> Product Name</label>
+          </div>
+          <Input
+            className="w-full h-[48px] hover:border-green-500 active:border-green-600"
+            placeholder="Please enter Product Name"
+            value={productName}
+            required={true}
+            onChange={(e) => setProductName(e.target.value)}
+          />
+        </div>
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> Old (Market) Price </label>
+          </div>
+          <Input
+            className="w-full h-[48px] hover:border-green-500 active:border-green-600"
+            placeholder="Old Price"
+            value={oldPrice}
+            required={true}
+            onChange={(e) => setOldPrice(e.target.value)}
+          />
+        </div>
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> New (Spark) Price </label>
+          </div>
+          <Input
+            className="w-full h-[48px] hover:border-green-500 active:border-green-600"
+            placeholder="New Price"
+            value={newPrice}
+            required={true}
+            onChange={(e) => setNewPrice(e.target.value)}
+          />
+        </div>
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> Available Quantity </label>
+          </div>
+          <Input
+            className="w-full h-[48px] hover:border-green-500 active:border-green-600"
+            placeholder="Available Quantity"
+            value={quantity}
+            required={true}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+        </div>
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> Unit (Kg, Pieces, Packs) </label>
+          </div>
+          <select
+            className="w-full px-2 rounded-md h-[48px] border hover:border-green-500 active:border-green-600"
+            value={unit}
+            required={true}
+            onChange={(e) => setUnit(e.target.value)}
+          >
+            <option value="">Select Unit</option>
+            <option value="Kg">Kg</option>
+            <option value="Bags">Bags</option>
+            <option value="Pieces">Pieces</option>
+            <option value="Keg">Keg</option>
+            <option value="Pack">Pack</option>
+            <option value="Box">Box</option>
+          </select>
+        </div>
 
-    <div
-      className={`${
-        isAdditionValid
-          ? "bg-green hover:bg-[#0f5c2e] text-white"
-          : "bg-gray-200 text-gray-500"
-      } text-[16px] w-full mt-8 text-center cursor-pointer capitalize  px-8 py-4 rounded-md w-fit`}
-      onClick={isAdditionValid ? handleSubmit : undefined}
-    >
-      {" "}
-      Add Deal
-    </div>
-  </div>);
-    
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> Deal Start Date</label>
+          </div>
+          <DatePicker
+            className="w-full h-[48px]"
+            onChange={(date, dateString) => handleStartDateChange(dateString)}
+          />
+        </div>
+        <div className="mt-4">
+          <div className="mb-1">
+            <label className="text-gray-500"> Deal Expiry Date</label>
+          </div>
+          <DatePicker
+            className="w-full h-[48px]"
+            onChange={(date, dateString) => handleEndDateChange(dateString)}
+          />
+        </div>
+
+        <div
+          className={`${
+            isAdditionValid
+              ? "bg-green hover:bg-[#0f5c2e] text-white"
+              : "bg-gray-200 text-gray-500"
+          } text-[16px] w-full mt-8 text-center cursor-pointer capitalize  px-8 py-4 rounded-md w-fit`}
+          onClick={isAdditionValid ? handleSubmit : undefined}
+        >
+          {" "}
+          Add Deal
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -276,7 +313,9 @@ const AdminPage = () => {
       <div className="mx-[16px] md:mx-[64px] bg-white flex justify-between mt-4 md:mt-32 py-4 items-center px-[16px] md:px-[32px] rounded-lg">
         <h2 className="text-[24px] font-medium">Admin Page</h2>
         <div
-          className="text-[16px] w-content text-center cursor-pointer capitalize bg-green hover:bg-[#0f5c2e] px-8 py-4 rounded-md text-white w-fit"
+          className={`${
+            useDrawer && "hidden"
+          } text-[16px] w-content text-center cursor-pointer capitalize bg-green hover:bg-[#0f5c2e] px-8 py-4 rounded-md text-white w-fit`}
           onClick={() => {
             setModalVisible(true);
             setOpenModal("addDeal");
@@ -285,6 +324,17 @@ const AdminPage = () => {
           {" "}
           Add new Deal
         </div>
+      </div>
+      <div
+        className={`${
+          !useDrawer && "hidden"
+        } fixed bottom-8 right-8 z-10 text-[16px] text-center cursor-pointer capitalize bg-green hover:bg-[#0f5c2e] px-8 py-8 rounded-[80px] text-white w-fit`}
+        onClick={() => {
+          setModalVisible(true);
+          setOpenModal("addDeal");
+        }}
+      >
+        <FaPlus size={18} />
       </div>
       <div className="flex px-[16px] md:mx-[64px] mt-4 md:mt-8 w-full">
         {" "}
@@ -414,25 +464,81 @@ const AdminPage = () => {
             </table>
           </div>
 
-          <div className={`${!useDrawer && "hidden"}`}>
+          <div className={`${!useDrawer && "hidden"} mt-4`}>
             {allDocs.map((product) => (
               <div
                 // key={index}
-                className="mx-[16px] flex items-center mt-3 bg-white rounded-lg px-4 py-2"
+                className="mx-[16px] flex items-center mt-3 bg-white rounded-lg px-4 py-4"
               >
                 <div className="w-full">
-                  <div className="w-full text-[16px] font-medium mb-1">
-                    {product.productName}
+                  <div className="flex items-center text-gray-700">
+                    <div className="w-full truncate text-[20px] font-medium mb-1">
+                      {product.productName}
+                    </div>
+                    <div
+                      className=" flex place-content-end"
+                      onClick={() => {
+                        handleSetMenuToggle(product.id);
+                      }}
+                    >
+                      <FaEllipsisV className="text-gray-700" />
+                    </div>
+                    {menuToggle === product.id
+                      ? menuVisible && (
+                          <ul className="absolute right-12 z-10 py-2 mt-2 bg-white border rounded-md shadow-md">
+                            {/* <li
+                              onClick={() => {
+                                // handleOpenModal(trip, "info");
+                              }}
+                              className="px-4 py-2 font-medium text-gray-700 border-b hover:bg-gray-100"
+                            >
+                              View
+                            </li>
+                            <li
+                              onClick={() => {
+                                // handleOpenModal(trip, "edit");
+                              }}
+                              className="px-4 py-2 font-medium text-gray-700 border-b hover:bg-gray-100"
+                            >
+                              Edit
+                            </li> */}
+                            <li
+                              onClick={() => {
+                                handleOpenDeleteModal(product);
+                              }}
+                              className="px-4 py-2 font-medium text-gray-700 border-b hover:bg-gray-100"
+                            >
+                              Delete
+                            </li>
+                          </ul>
+                        )
+                      : ""}
                   </div>
-
-                  <div className="flex items-center">
-                    <p className="text-[14px] text-gray-500">
+                  <div className="flex items-center mt-4">
+                    <p className="text-[16px] text-gray-500 mr-2">
                       GH₵ {product.currentPrice}
                     </p>
-                    <div className="mx-2 rounded-[12px] h-1 w-1 bg-gray-300"></div>
-                    <p className="text-[14px] text-gray-500">
-                      {product.availableQTY} {product.measurement} Available
-                    </p>
+                    <div className=" w-fit text-[14px] text-[#327531] border border-[#A4FF8D] bg-[#CAFFC1] px-[8px] py-[2px] rounded-sm">
+                      {product.discount}% off
+                    </div>
+                  </div>
+                  <p className="text-[16px] text-gray-500 mt-1">
+                    {product.availableQTY} {product.measurement} Available
+                  </p>
+
+                  <div className="flex mt-6 gap-[12px] w-full">
+                    <div className="w-full bg-gray-50 border rounded-md p-2">
+                      <p className="text-[13px] text-gray-400">Deal Starts</p>
+                      <p className="text-[16px] text-gray-700 mt-1">
+                        {formatDate(product.startDate)}
+                      </p>
+                    </div>
+                    <div className="w-full bg-gray-50 border rounded-md p-2">
+                      <p className="text-[13px] text-gray-400">Deal Expires</p>
+                      <p className="text-[16px] text-gray-700 mt-1">
+                        {formatDate(product.expiryDate)}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -571,6 +677,41 @@ const AdminPage = () => {
         >
           {
             <div className="w-full text-center place-items-center">
+              <FaExclamationCircle
+                size={32}
+                className="text-[#E71D36] w-full mt-8"
+              />
+              <div className="mt-4 text-base font-medium boder-b">
+                Delete {modalData.productName}?
+              </div>
+
+              <div className=" mt-8">
+                <div
+                  className="w-full text-[16px] w-content text-center cursor-pointer capitalize bg-green hover:bg-[#0f5c2e] px-8 py-4 rounded-md text-white w-fit"
+                  onClick={() => {
+                    handleDeleteDoc(modalData.id);
+                  }}
+                >
+                  Delete
+                </div>
+                <div className="mt-6 cursor-pointer">Cancel</div>
+              </div>
+            </div>
+          }
+        </Modal>
+      )}
+      {modalVisible && openModal === "success  " && (
+        <Modal
+          open={modalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          centered={true}
+          footer={false}
+          closable={true}
+          width={240}
+        >
+          {
+            <div className ="w-full text-center place-items-center">
               <FaExclamationCircle
                 size={32}
                 className="text-[#E71D36] w-full mt-8"
